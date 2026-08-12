da4c1c1e9e7ff02d260b8b97c3572772e2e31812
Ardila-JC7 <julian.ardila797@pascualbravo.edu.co>
2026-08-12 13:17:36 -0500

    Update frutas_app.py

--- frutas_app.py
+++ frutas_app.py
@@ -19,6 +19,7 @@ st.write("Vector de tu fruta:", fruta_usuario)
 manzana = [170, 7.0, 7]
 banano = [120, 5.0, 9]
 naranja = [200, 8.0, 6]
+cereza = [7, 2.0, 13]
 
 # Calculamos las distancias
 
@@ -40,18 +41,26 @@ distancia_naranja = math.sqrt(
     (fruta_usuario[2] - naranja[2])**2
 )
 
+distancia_cereza = math.sqrt(
+    (fruta_usuario[0] - cereza[0])**2 +
+    (fruta_usuario[1] - cereza[1])**2 +
+    (fruta_usuario[2] - cereza[2])**2
+)
+
 # Mostramos las distancias
 st.subheader("Distancias")
 
 st.write("🍎 Manzana:", distancia_manzana)
 st.write("🍌 Banano:", distancia_banano)
 st.write("🍊 Naranja:", distancia_naranja)
+st.write("🍒 Cereza:", distancia_cereza)
 
 # Buscamos la distancia menor
 distancias = {
     "🍎 Manzana": distancia_manzana,
     "🍌 Banano": distancia_banano,
-    "🍊 Naranja": distancia_naranja
+    "🍊 Naranja": distancia_naranja,
+    "🍒 Cereza": distancia_cereza
 }
 
 fruta_mas_parecida = min(distancias, key=distancias.get)
